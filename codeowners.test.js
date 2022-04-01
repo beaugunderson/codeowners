624b99d98828d499a59f76619a6e41f80715d9a9
Nathan Pastor <nathan.pastor@teacherspayteachers.com>
2022-04-01 13:09:12 -0400

    Here are some tests, while I'm here

---
+++ codeowners.test.js
@@ -0,0 +1,18 @@
+/* global describe it expect */
+const Codeowners = require('./codeowners.js');
+
+const repos = new Codeowners();
+
+describe('codeowners', () => {
+  it('returns owners for file', () => {
+    const owner = repos.getOwner(__filename);
+    expect(owner).toEqual(['@beaugunderson']);
+  });
+
+  it('owners is a copy of internal data', () => {
+    repos.getOwner(__filename).pop();
+
+    const owner = repos.getOwner(__filename);
+    expect(owner).toEqual(['@beaugunderson']);
+  });
+});
