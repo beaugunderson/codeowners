fdefe0793272b6a6d788fa53d117dc68f3d67ae6
Noah Manneschmidt <noah@manneschmidt.net>
2024-09-19 12:53:38 -0700

    add bitbucket parent folder

--- src/codeowners.ts
+++ src/codeowners.ts
@@ -12,7 +12,7 @@ function ownerMatcher(pathString: string) {
   return matcher.ignores.bind(matcher);
 }
 
-const PARENT_FOLDERS = [".github", ".gitlab", "docs"];
+const PARENT_FOLDERS = [".bitbucket", ".github", ".gitlab", "docs"];
 const CODEOWNERS = "CODEOWNERS";
 
 interface OwnerEntry {
